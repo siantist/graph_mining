6f5f015c7f83b0b47d17239efe08cb480f7c1036
siantist <104287799+siantist@users.noreply.github.com>
2022-04-23 20:18:17 -0700

    Create diameter.py

---
+++ properties/diameter.py
@@ -0,0 +1,5 @@
+# properties: diameter d: max len of all shortest paths found
+# btwn any pairs of nodes in (subgraph G' or graph G)
+
+def diameter(g_shortest_paths_mat):
+    return max(g_shortest_paths_mat)
