2c848058acd7f030427f8f8907145878bf190a96
siantist <104287799+siantist@users.noreply.github.com>
2022-06-14 16:14:36 -0400

    Update reverse_cutmckee.py

--- grakel/node_reordering/reverse_cutmckee.py
+++ grakel/node_reordering/reverse_cutmckee.py
@@ -1,7 +1,23 @@
 # node reordering
+
+def sort_adjacency_i(alist, degrees): # degrees is dictionary
+    degs = []
+    
+    for v in alist:
+        vdeg = degrees[v]
+        degs.append(vdeg)
+        
+    sted_degs = degs.argsort()
+    
+    sted_list = alist[sted_degs]
+    
+    return sted_list
+
+
 # adjacency is dictionary: int -> vector(int)
 # degrees is dict: int -> int
 #p, q are int's
+
 def RCM(adjacency, degrees, P, Q):
     n = len(adjacency)
     R = []
@@ -10,21 +26,16 @@ def RCM(adjacency, degrees, P, Q):
         if i > len(R):
             R.append(Q) 
             #push!(R, Q)
-            A = sort_adjacency_by_degrees(adjacency[Q])
-            for b in A
+            A = sort_adjacency_i(adjacency[Q])
+            for b in A:
                 if b not in R:
                     R.append(b)
-                end
-            end
-        else
-            t = sort_adjacency_by_degrees(adjacency[R[i]])
+                
+        else:
+            t = sort_adjacency_i(adjacency[R[i]])
             for T in t:
                 if T not in R:
                     R.append(T)
-                end
-            end
-        end
-    end
+
     new_order = reverse(R)
     return new_order
-end
