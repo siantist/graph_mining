d000ce9a27ced83e09df11a35f975480fc9f862e
siantist <104287799+siantist@users.noreply.github.com>
2022-07-31 19:42:54 -0400

    Create pref_rw_setup.py

---
+++ grakel/method_random_walk/pref_rw_setup.py
@@ -0,0 +1,16 @@
+
+# define the weight, given input vector q (quantity assoc to node i)
+
+def weights(A, q, beta):
+    n = len(A)
+    w = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            w[i,j] = np.pow(q[i]*q[j], beta)*A[i,j]
+    
+    return w
+
+ # take the kronecker product of the weight matrices
+
+
+np.kron(w1, w2)
