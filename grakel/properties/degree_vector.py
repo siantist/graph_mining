68bf93ba0ba325a55eedd4cb792754d6e59b0d22
siantist <104287799+siantist@users.noreply.github.com>
2022-09-30 02:27:44 -0400

    Update degree_vector.py

--- grakel/properties/degree_vector.py
+++ grakel/properties/degree_vector.py
@@ -5,3 +5,13 @@ def degree_vector(A):
         degrees.append(s)
         
     return degrees
+
+from tu dataset to nx graph
+def degree_vector(A):
+    degrees = []
+    for row in A:
+        row = np.array(row).flatten()
+        s = sum(row)
+        degrees.append(s)
+        
+    return degrees
