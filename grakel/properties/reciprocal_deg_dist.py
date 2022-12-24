4d083dab20677feda6e0501423b19855e66cd41e
siantist <104287799+siantist@users.noreply.github.com>
2022-12-23 22:01:01 -0500

    Create reciprocal_deg_dist.py

---
+++ grakel/properties/reciprocal_deg_dist.py
@@ -0,0 +1,13 @@
+# note: how to speed up with python-graphblas
+# reciprocal degree distance
+def reciprocal_deg_dist(G, shortest_path_matrix):
+  n = len(G)
+  # compute the sum
+  s =0 
+  for i in range(n):
+    for j in range(n):
+      if i != j:
+        ideg = sum(G[i])
+        jdeg = sum(G[j])
+        s= s+ (ideg+jdeg)/shortest_path_matrix[i,j]
+  return s 
