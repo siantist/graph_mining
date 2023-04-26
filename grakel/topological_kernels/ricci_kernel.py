fbc69b2aee3eed1ccfc71d4e68b9481cfb57ed28
siantist <104287799+siantist@users.noreply.github.com>
2023-04-26 19:52:33 -0400

    Create ricci_kernel.py

---
+++ grakel/topological_kernels/ricci_kernel.py
@@ -0,0 +1,8 @@
+# https://arxiv.org/pdf/1907.07129.pdf
+"""
+Use the edge curvature distribution to form a graph kernel which uses purely the
+graph topology and thereby works for settings when node attributes are not available
+
+Ollivier-Ricci curvature is 1 - W(u,v)/d(u,v) 
+
+"""
