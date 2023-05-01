5781b2a823733c3404510249c53f9c6fcc932568
siantist <104287799+siantist@users.noreply.github.com>
2023-05-01 19:19:16 -0400

    Update ricci_kernel.py

--- grakel/topological_kernels/ricci_kernel.py
+++ grakel/topological_kernels/ricci_kernel.py
@@ -10,7 +10,24 @@ W is earth mover distance
 https://github.com/saibalmars/GraphRicciCurvature
 
 https://github.com/saibalmars/GraphRicciCurvature/blob/master/GraphRicciCurvature/OllivierRicci.py
+
+!pip install GraphRicciCurvature
 """
+
+import networkx as nx
+from GraphRicciCurvature.OllivierRicci import OllivierRicci
+from GraphRicciCurvature.FormanRicci import FormanRicci
+
+
+def ricci_kernel(G1, G2):
+  orc1 = OllivierRicci(G1, alpha=0.5, verbose="INFO")
+  orc2 = OllivierRicci(G2, alpha=0.5, verbose="INFO")
+  orc1.compute_ricci_curvature()
+  orc2.compute_ricci_curvature()
+  c1 = orc1.G[0][1]["ricciCurvature"]
+  c2 = orc2.G[0][1]["ricciCurvature"]
+  
+
 def curvature_distribution(G, m): # m is the distribution on nbd of nodes
   for edge in G.edges:
     # calculate W(u,v)
