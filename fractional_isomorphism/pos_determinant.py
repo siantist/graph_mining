9477602e429baae45e630839c55c6b062a07f22d
siantist <104287799+siantist@users.noreply.github.com>
2023-07-03 20:35:35 -0700

    Update pos_determinant.py

--- fractional_isomorphism/pos_determinant.py
+++ fractional_isomorphism/pos_determinant.py
@@ -1,18 +1,17 @@
 # from On the maximal solution of a linear system over tropical semirings
 # pos determinant
 
-# input: set of permutations s
-def pos_determinant(s):
+# input: set of permutations, matrix A
+# tropical determinant 
+def pos_determinant(A, permutations):
     tot_sum = 0
-    for p in s:
-        # take sum
-
-        sigma= p
-        prod = 1
-        # take product
-        for i in range(n)
-            prod = prod*A[i, sigma(i)]
-
-        tot_sum = tot_sum + prod
-
+    for p in permutations:
+      tot_prod = 1
+      for ind in range(len(A)):
+        pi = permutations[ind]
+        aij = A[ind, pi]
+        #mult 
+        tot_prod = tot_prod*aij 
+      tot_sum = tot_sum + tot_prod
+    
     return tot_sum
