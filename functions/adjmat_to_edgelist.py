7d0b493f39e1077da54cc1be89ab15093a050c55
siantist <104287799+siantist@users.noreply.github.com>
2023-07-07 18:51:37 -0700

    Create adjmat_to_edgelist.py

---
+++ functions/adjmat_to_edgelist.py
@@ -0,0 +1,17 @@
+def adjmat_to_edgelist(A):
+  elist = []
+  ind1 = 0
+  for row in A:
+    # add to
+    # check permutation exists
+    ind2 =0
+    for el in row:
+      pair1 = (ind1, ind2)
+      pair2 = (ind2, ind1)
+      if pair1 not in elist:
+        if pair2 not in elist:
+          elist.append(pair1)
+      ind2 = ind2 + 1
+    ind1 = ind1+1
+
+  return elist
