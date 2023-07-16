b45a8ef698010d50b397a9bd1338410dc2a97d69
siantist <104287799+siantist@users.noreply.github.com>
2023-07-16 15:04:31 -0400

    Update matrix_multiplication.py

--- Tropical_semiring/matrix_multiplication.py
+++ Tropical_semiring/matrix_multiplication.py
@@ -13,7 +13,21 @@ def mult_tropical_min(A, B): # matrix A, B
 			C[i,j] = min(cvec)
 	
 	return C 
-	
+
+# for A times vector x in (n,1) 
+def mult_tropical_min_vec(A, x): 
+	n = len(A)
+	C = np.zeros((n,1))
+	for i in range(n):
+		arow = A[i,:]
+		avec = []
+		for j in range(n):
+			xj = x[j]
+			avec.append(arow[j] + xj) 
+		C[i] = min(avec) 
+	return C
+			
+
 # mult is add, add is max 
 def mult_tropical_max(A, B):
 	n = len(A)
