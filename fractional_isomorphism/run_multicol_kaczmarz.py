acf4c774bac91b6aa5f884b4e7a2f62dd30dbef8
siantist <104287799+siantist@users.noreply.github.com>
2023-10-25 16:42:31 -0400

    Update run_multicol_kaczmarz.py

--- fractional_isomorphism/run_multicol_kaczmarz.py
+++ fractional_isomorphism/run_multicol_kaczmarz.py
@@ -2,3 +2,19 @@ import kaczmarz
 b = B[:,1]
 c= kaczmarz.Cyclic.solve(A, b) # cyclic selection rule
 c1 = kaczmarz.MaxDistance.solve(A, b) # max dist selection rule
+
+# test frac isomorphism <-> complete graphs? (what does self edges do? / deleting diagonal?)
+A = [[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]]
+B = [[1,1,1,0],[1,0,1,1],[1,1,0,1],[0,1,1,1]]
+import numpy as np
+X = np.identity(4)
+
+ax = np.matmul(A,X)
+bx = np.matmul(B,X)
+
+A = np.array(A)
+B = np.array(B)
+
+C = multicol_kaczmarz(A,B)
+
+#https://pypi.org/project/kaczmarz-algorithms/
