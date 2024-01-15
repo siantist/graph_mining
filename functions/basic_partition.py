c3f845d487def207181d341a4418a14988ef03fd
siantist <104287799+siantist@users.noreply.github.com>
2024-01-14 17:16:59 -0800

    Create basic_partition.py

---
+++ functions/basic_partition.py
@@ -0,0 +1,12 @@
+# basic partition of given ordering of vertices
+def basic_partition(G, m): # m is number of sets
+  n = len(G)
+  vertex_sets =[]
+  vertex_sets_map = {}
+  s = np.floor(n/m)
+  for i in range(m):
+    ilist= np.arange(i*s, i*s+s)
+    vertex_sets.append(ilist)
+    vertex_sets_map[i] = ilist
+
+  return vertex_sets, vertex_sets_map
