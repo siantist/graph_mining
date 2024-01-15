42ef2024390974235f927645bf9879e50e66218c
siantist <104287799+siantist@users.noreply.github.com>
2024-01-14 23:49:14 -0800

    Update basic_partition.py

--- functions/basic_partition.py
+++ functions/basic_partition.py
@@ -11,3 +11,14 @@ def basic_partition(G, m): # m is number of sets
     vertex_sets_map[i] = ilist
 
   return vertex_sets, vertex_sets_map
+  
+def ed(X, Y, A): # adj matrix 
+  # count the num edges between X, Y
+  num= 0
+  for i in X:
+    for j in Y:
+      Aij = A[i,j]
+      if Aij ==1 and i!=j and i<j: # only count once
+        num =num+1
+  denom = len(X)*len(Y)
+  return num/(denom)
