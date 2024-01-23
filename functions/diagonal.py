8946119664d69ad45b8fcac85c89bc7144545fcf
siantist <104287799+siantist@users.noreply.github.com>
2024-01-22 23:19:09 -0800

    Update diagonal.py

--- functions/diagonal.py
+++ functions/diagonal.py
@@ -33,3 +33,29 @@ def sum_diag(A, j, eps):
   # compare to trace 
   t = np.matrix.trace(A)
   return s, t
+
+#scale each row by the stationary dist?
+def scale_by_stationary(A, pi):
+  n = len(A)
+  B = np.zeros(n,n)
+  for i in range(len(pi)):
+    p = pi[i]
+    B[i] = p*A[i]
+  return B
+
+# turn stationary distribution into a diagonal matrix
+def diag_mat(pi):
+  n = len(pi)
+  D = np.zeros((n,n))
+  for i in range(n):
+    D[i,i] = pi[i]
+  return D
+# determine the stationary dist based on diff graph invariants
+def det_stat_dist(A):#, Ainvariant): # Ainvariant matrix is matrix of inv
+  Atranspose = A.T
+  eigenvals, eigenvects = np.linalg.eig(A)
+  close_to_1idx = np.isclose(eigenvals, 1)
+  target_eigenvect = eigenvects[:, close_to_1idx]
+  target_eigenvec = target_eigenvect[:,0]
+  stat_dist = target_eigenvec/sum(target_eigenvec)
+  return stat_dist
