8bc4be99abec4bc16864f40ca661adfe704b1b32
siantist <104287799+siantist@users.noreply.github.com>
2024-03-21 13:10:28 -0700

    Update connectivity_index_matrix.py

--- matrices/network_topology_matrices/connectivity_index_matrix.py
+++ matrices/network_topology_matrices/connectivity_index_matrix.py
@@ -1,4 +1,4 @@
-def connectivity_index_matrix(A): # adjacency matrix
+def connectivity_index_matrix(A, edges, power=2): # adjacency matrix
   degrees = degree_matrix(A)
   a = connectivity_index_nodes(degrees, edges, power)
   # create diagonal matrix 
