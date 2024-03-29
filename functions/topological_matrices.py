f2c8cbbbe541965c52eb0bdf8c94af1154f36b7f
siantist <104287799+siantist@users.noreply.github.com>
2024-03-29 11:32:48 -0700

    Update topological_matrices.py

--- functions/topological_matrices.py
+++ functions/topological_matrices.py
@@ -1,4 +1,5 @@
 from graph_mining.functions.topological_indices_nodes import zagreb_m1_index_nodes, zagreb_m2_index_nodes, connectivity_index_nodes, augmented_zagreb_index_nodes, eccentric_connectivity_index_nodes
+from graph_mining.functions.degrees_of_nodes import degrees_of_nodes
 
 import numpy as np
 def degree_matrix(A):
@@ -7,6 +8,17 @@ def degree_matrix(A):
   for row in A:
     degs.append(np.sum(row))
   return degs
+
+
+def deg_sum_matrix(A):
+  import numpy as np
+  n = len(A)
+  degsums = np.zeros((n,n))
+  degrees = degrees_of_nodes(A)
+  for i in range(n):
+    for j in range(n):
+      degsums[i,j] = degrees[i] + degrees[j]
+  return degsums
   
 def connectivity_index_matrix(A, edges, power=2): # adjacency matrix
   import numpy as np
